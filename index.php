16b882c070e3343ba6bbdb2407859654c5ff4812
filipljoljic <filip.ljoljic@stu.ibu.edu.ba>
2023-11-14 14:32:29 +0100

    Test routes: 	new file:   .htaccess 	modified:   index.php

--- index.php
+++ index.php
@@ -1,7 +1,17 @@
 <?php
 
-echo 'Hellooooo';
+require "vendor/autoload.php";
 
+Flight::route("/", function(){
+    echo "Hello flight";
+});
+
+Flight::route("GET /test", function(){
+    echo "test flight";
+
+});
+
+Flight::start();
 ?>
 
 
