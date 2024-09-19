edfbb4da83480e8b57f6d7364e305aac0cd3e99e
Filip Ljoljic <you@example.com>
2024-09-19 10:37:04 +0200

    corserror7

--- rest/index.php
+++ rest/index.php
@@ -1,5 +1,6 @@
 <?php
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: https://orca-app-ispuh.ondigitalocean.app");
+header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
