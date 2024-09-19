5ddb2bf6dc5bd813a24c94ec98043a807305df61
Filip Ljoljic <you@example.com>
2024-09-19 12:32:35 +0200

    corserror11

--- rest/services/LeagueMembersService.php
+++ rest/services/LeagueMembersService.php
@@ -3,8 +3,10 @@ require_once 'BaseService.php';
 require_once __DIR__."/../dao/LeagueMembersDao.class.php";
 
 class LeagueMembersService extends BaseService{
-    public function __construct(){
-        $this->dao = new LeagueMembersDao(); // Instantiate the DAO here
+    private $dao;  // Explicitly declare the property
+
+    public function __construct() {
+        $this->dao = new LeagueMembersDao();  // Instantiate the DAO here
         parent::__construct($this->dao);  // Pass it to the parent constructor
     }
 
