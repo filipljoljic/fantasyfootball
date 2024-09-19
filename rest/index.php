b52c7e7e0c0ccaa4f3cf7c753b073f24eec86920
Filip Ljoljic <you@example.com>
2024-09-19 09:52:55 +0200

    corserror6

--- rest/index.php
+++ rest/index.php
@@ -3,7 +3,6 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
-
 require_once "../vendor/autoload.php";
 
 require_once "dao/UsersDao.class.php";
