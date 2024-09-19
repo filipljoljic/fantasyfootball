c323c9939e5eb589c8d0b5051c2c44ee0ba12ad8
Filip Ljoljic <you@example.com>
2024-09-19 09:29:10 +0200

    corserror3

--- rest/index.php
+++ rest/index.php
@@ -1,7 +1,9 @@
 <?php
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: https://orca-app-ispuh.ondigitalocean.app");
+header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
 
 
 require_once "../vendor/autoload.php";
